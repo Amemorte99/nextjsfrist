53a7698296d3818e54d2ea23b51e2da90fc9f0ac
Amemorete <koisonnmanes@gmail.com>
2023-05-23 10:06:03 +0000

    oui

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
     return NextResponse.json(todos)
 } 
 
-const Api_key: string = process.env.DATA_API_key
+const Api_key: string = "DaveGrayTeachesCode"
 
 export async function DELETE(resquet:Request) {
 
@@ -30,11 +30,63 @@ export async function DELETE(resquet:Request) {
             'API-Key':Api_key
         }
 
-    })
-
-   
+    }) 
 
     return NextResponse.json({
         "message": 'Todo ${id} à été supprimer'
+    })
+} 
+
+
+
+export async function POST(resquet:Request) {
+
+    const {userId,title} : Partial<Todo>= await resquet.json()
+
+    if(!userId || !title) return NextResponse.json({"message":"Missing required data"})
+
+    const res =await fetch(DATA_SOURCE_URL,{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'API-Key':Api_key
+        },
+        body: JSON.stringify({
+
+            userId,title,completed:false
+        })
+
+    }) 
+    const newTodo: Todo =await res.json()
+
+    return NextResponse.json({
+       newTodo
+    })
+} 
+
+
+
+export async function PUT(resquet:Request) {
+
+    const {userId,id,title,completed} : Todo= await resquet.json()
+
+    if(!userId || !id || !title || typeof(completed)!=='boolean') return NextResponse.json({"message":"Missing required data","status":false})
+
+    const res =await fetch(`${DATA_SOURCE_URL}/${id}`,{
+        method:'PUT',
+        headers: {
+            'Content-Type':'application/json',
+            'API-Key':Api_key
+        },
+        body: JSON.stringify({
+
+            userId,title,completed
+        })
+
+    }) 
+    const updateTodo: Todo =await res.json()
+
+    return NextResponse.json({
+        updateTodo,"status":true
     })
 } 
